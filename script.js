dee269c2a492aad3333f4574efeecc8920e8b533
samian <sam.iannazzo@yahoo.com>
2025-11-25 19:15:26 -0800

    answered some of the questions

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ console.log("One thing that helped me a lot during this class where the weekly l
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("Personally the one thing I would change is how long the class was. For me the 8 weeks definitely was very challenging, if I could go back I would have taken javascript and css in different semesters. ");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I was unsure what javascript was for, so pretty much everything I have learned in this class was new to me, very challenging but hopefully will get the hang of it better next time.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("All of the concepts in our javascript class where new to me since this is my first time taking any classes in this field. The most interesting concept to me had to be ");
